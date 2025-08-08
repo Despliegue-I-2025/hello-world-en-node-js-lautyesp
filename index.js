e514b8b7cee511cd19ec2b9426349d48a61728cf
lautyesp <lautyespinosa9@gmail.com>
2025-08-08 19:47:53 -0300

    Add files via upload

--- index.js
+++ index.js
@@ -1,14 +1,13 @@
 const http = require('http');
 
 const server = http.createServer((req, res) => {
-  res.setHeader('Content-Type', 'text/plain; charset=utf-8');
-
-  // Esta línea está comentada. Deberías descomentarla para que funcione
-  // y cambiar el texto que se envía como respuesta.
-
-  //res.end('Poné lo que quieras acá');
+  res.statusCode = 200;
+  res.setHeader('Content-Type', 'text/plain');
+  res.end('lauty espinosa');
 });
 
-server.listen(3000, () => {
-  console.log('Servidor escuchando en http://localhost:3000');
+const PORT = 3000;
+
+server.listen(PORT, () => {
+  console.log(`Servidor corriendo en http://localhost:${PORT}`);
 });
